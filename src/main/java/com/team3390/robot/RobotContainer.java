a7d03646b635b2534b9d255759bf35fca7567ddd
CanOkan <tamadaminasordun@gmail.com>
2024-01-07 12:19:04 +0300

    Initial Commit

---
+++ src/main/java/com/team3390/robot/RobotContainer.java
@@ -0,0 +1,20 @@
+/**
+ * @author team3390, canokan
+ */
+
+package com.team3390.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class RobotContainer {
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  private void configureBindings() {}
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
