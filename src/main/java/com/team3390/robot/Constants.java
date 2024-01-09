de1ff4c5712372fe67a85b6d47dd6c207bbac78a
TamAdaminaSordun <72205789+TamAdaminaSordun@users.noreply.github.com>
2024-01-09 18:53:54 +0300

    Drivetrain v1

---
+++ src/main/java/com/team3390/robot/Constants.java
@@ -0,0 +1,26 @@
+package com.team3390.robot;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.I2C.Port;
+
+public final class Constants {
+  
+  public static final boolean ROBOT_FIELD_MODE = DriverStation.isFMSAttached();
+  public static final boolean TUNING_MODE = false;
+
+  public static final int JOYSTICK_LEFT_PORT = 0;
+  public static final int JOYSTICK_RIGHT_PORT = 1;
+  public static final int JOYSTICK_GAMEPAD_PORT = 4;
+
+  public static final int     DRIVE_FRONT_LEFT_ID = 0;
+  public static final int     DRIVE_FRONT_RIGHT_ID = 1;
+  public static final int     DRIVE_REAR_LEFT_ID = 2;
+  public static final int     DRIVE_REAR_RIGHT_ID = 3;
+  public static final boolean DRIVE_FRONT_LEFT_INVERTED = false;
+  public static final boolean DRIVE_FRONT_RIGHT_INVERTED = true;
+  public static final boolean DRIVE_REAR_LEFT_INVERTED = false;
+  public static final boolean DRIVE_REAR_RIGHT_INVERTED = true;
+
+  public static final Port SENSOR_NAVX_PORT = Port.kMXP;
+
+}
---
+++ src/main/java/com/team3390/robot/subsystems/Drivetrain.java
@@ -0,0 +1,92 @@
+package com.team3390.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.kauailabs.navx.frc.AHRS;
+import com.team3390.lib.drivers.LazyTalonSRX;
+import com.team3390.lib.drivers.TalonSRXCreator;
+import com.team3390.lib.drivers.TalonSRXCreator.Configuration;
+import com.team3390.robot.Constants;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+
+  private static Drivetrain instance;
+
+  private boolean isBreakMode = false;
+
+  private final Configuration talonConfiguration = new Configuration();
+  private final LazyTalonSRX frontLeft, frontRight, rearLeft, rearRight;
+  private final MecanumDrive driveController;
+
+  private final AHRS navX = new AHRS(Constants.SENSOR_NAVX_PORT);
+
+  public synchronized static Drivetrain getInstance() {
+    if (instance == null) {
+      instance = new Drivetrain();
+    }
+    return instance;
+  }
+
+  public Drivetrain() {
+    talonConfiguration.NEUTRAL_MODE = isBreakMode ? NeutralMode.Brake : NeutralMode.Coast;
+    frontLeft = TalonSRXCreator.createTalon(Constants.DRIVE_FRONT_LEFT_ID, talonConfiguration);
+    frontRight = TalonSRXCreator.createTalon(Constants.DRIVE_FRONT_RIGHT_ID, talonConfiguration);
+    rearLeft = TalonSRXCreator.createTalon(Constants.DRIVE_REAR_LEFT_ID, talonConfiguration);
+    rearRight = TalonSRXCreator.createTalon(Constants.DRIVE_REAR_RIGHT_ID, talonConfiguration);
+
+    frontLeft.setInverted(Constants.DRIVE_FRONT_LEFT_INVERTED);
+    frontRight.setInverted(Constants.DRIVE_FRONT_RIGHT_INVERTED);
+    rearLeft.setInverted(Constants.DRIVE_REAR_LEFT_INVERTED);
+    rearRight.setInverted(Constants.DRIVE_REAR_RIGHT_INVERTED);
+
+    // MotorController frontLeftMotor, MotorController rearLeftMotor, MotorController frontRightMotor, MotorController rearRightMotor
+    driveController = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+  }
+
+  @Override
+  public void periodic() {}
+
+  public double getHeading() {
+    return navX.getAngle();
+  }
+
+  public Rotation2d getHeading2d() {
+    return new Rotation2d(Math.toRadians(navX.getAngle()));
+  }
+
+  /**
+   * Drive method for Mecanum platform.
+   *
+   * <p>Angles are measured counterclockwise from the positive X axis. The robot's speed is
+   * independent of its angle or rotation rate.
+   * @param xSpeed The robot's speed along the X axis [-1.0..1.0]. Forward is positive.
+   * @param ySpeed The robot's speed along the Y axis [-1.0..1.0]. Left is positive.
+   * @param zRotation The robot's rotation rate around the Z axis [-1.0..1.0]. Counterclockwise is
+   *     positive.
+   */
+  public void driveCartesian(double xSpeed, double ySpeed, double zRotation) {
+    driveCartesian(xSpeed, ySpeed, zRotation, false);
+  }
+
+  /**
+   * Drive method for Mecanum platform.
+   *
+   * <p>Angles are measured counterclockwise from the positive X axis. The robot's speed is
+   * independent of its angle or rotation rate.
+   * @param xSpeed The robot's speed along the X axis [-1.0..1.0]. Forward is positive.
+   * @param ySpeed The robot's speed along the Y axis [-1.0..1.0]. Left is positive.
+   * @param zRotation The robot's rotation rate around the Z axis [-1.0..1.0]. Counterclockwise is
+   *     positive.
+   * @param fod Field-Oriented Driving
+   */
+  public void driveCartesian(double xSpeed, double ySpeed, double zRotation, boolean fod) {
+    if (fod) {
+      driveController.driveCartesian(xSpeed, ySpeed, zRotation, getHeading2d());
+    } else {
+      driveController.driveCartesian(xSpeed, ySpeed, zRotation);
+    }
+  }
+}
